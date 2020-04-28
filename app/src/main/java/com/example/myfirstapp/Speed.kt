7e5dcbd0ea76810c7634a95cf70f4c23237955e4
Bence <bencuka2903@gmail.com>
2020-04-28 17:45:41 +0200

    Creating Activity "Speed"
    - created activity with all the xml and kotlin files needed
    - added a button, label and a textfield

---
+++ app/src/main/java/com/example/myfirstapp/Speed.kt
@@ -0,0 +1,12 @@
+package com.example.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Speed : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speed)
+    }
+}
